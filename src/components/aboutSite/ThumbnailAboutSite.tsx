993ba63c9632cdc3691f4b843c9709c60b1ee4d5
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2024-01-24 14:22:16 +0700

    fix

--- src/components/aboutSite/ThumbnailAboutSite.tsx
+++ src/components/aboutSite/ThumbnailAboutSite.tsx
@@ -5,7 +5,7 @@ import ThumbnailAboutSite1 from './assets/unsplash_DE6rYp1nAho.svg'
 const ThumbnailAboutSite: React.FC = () => {
   return (
     <div tw="container relative [margin-left: -65px] ">
-      <img tw="w-full" src={ThumbnailAboutSite1} alt="thumbnailAboutSite" />
+      <img tw="w-full h-full" src={ThumbnailAboutSite1} alt="thumbnailAboutSite" />
       {/* Play button  */}
       <button tw="absolute top-[34%] left-[43%] max-md:scale-50 max-sm:[top: -1%] max-sm:[left: 24%]">
         <svg
