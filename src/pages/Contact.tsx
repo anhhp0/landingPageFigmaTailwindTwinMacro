806cfb76eb5899b39538873c824a35101140b5a5
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2024-01-25 17:45:33 +0700

    fix width img

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,12 +2,14 @@ import ContactUsContactSite from '../components/Contact/MainContactUsContactSite
 import Layout from '../layout'
 import Map from '../assets/map.svg'
 import GetInTouch from '../components/Contact/GetInTouch'
+import tw from 'twin.macro'
+
 
 const Contact = () => {
   return (
     <Layout>
       <ContactUsContactSite />
-      <img src={Map} />
+      <img src={Map} tw='mx-auto justify-center'/>
       <GetInTouch/>
     </Layout>
   )
