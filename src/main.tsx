d91187770ccbdb4496dd13951d116f7e0d505dfb
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2024-01-26 16:29:57 +0700

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,7 @@ import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/500.css'
 import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
-import Home1 from './pages/Home/Home'
-import About from './pages/About/About'
-import Service from './pages/Service/Service'
-import Gallery from './pages/Gallery/Gallery'
-import Team from './pages/Team/Team'
-import Blog from './pages/Blog/Blog'
-import Contact from './pages/Contact/Contact'
+
 import AllRoutes from './routes/AllRouter'
 
 const container = document.getElementById('root')
@@ -20,13 +14,6 @@ const root = createRoot(container!)
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    {/* <Home1 /> */}
-    {/* <About/> */}
-    {/* <Service/> */}
-    {/* <Gallery /> */}
-    {/* <Team /> */}
-    {/* <Blog /> */}
-    {/* <Contact/> */}
     <AllRoutes />
   </React.StrictMode>,
 )
