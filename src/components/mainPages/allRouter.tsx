80150666078c04a9af5e78a062ca727828dbda8f
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2024-02-15 10:33:03 +0700

    update

--- src/components/mainPages/allRouter.tsx
+++ src/components/mainPages/allRouter.tsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home1 from './Home1'
-import About from './About'
-import Service from './Service'
-import Gallery from './Gallery'
-import Blog from './Blog'
-import Contact from './Contact'
-import Team from './Team'
+import Home from '../pages/Home/Home'
+import About from '../pages/About/About'
+import Service from '../pages/Service/Service'
+import Gallery from '../pages/Gallery/Gallery'
+import Blog from '../pages/Blog/Blog'
+import Contact from '../pages/Contact/Contact'
+import Team from '../pages/Team/Team'
 
 const AllRoutes: React.FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route index element={<Home1 />} />
-      <Route path="/" element={<Home1 />} />
-      <Route path="/home" element={<Home1 />} />
+      <Route index element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/service" element={<Service />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/team" element={<Team />} />
-
       <Route path="/blog" element={<Blog />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
+    <SpeedInsights />
+    <Analytics />
   </BrowserRouter>
 )
 
